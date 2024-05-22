aa24b027690dcad570ff160269d416eb4b643cb7
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 00:41:23 +0100

    update no syntax

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,4 @@
 -- list the privileges given to specified users
 
--- query to list privilege for user user_0d_1 and user_0d_2
-SHOW GRANT FOR user_0d_1@localhost;
-SHOW GRANT FOR user_0d_2@localhost;
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+SHOW GRANT FOR 'user_0d_2'@'localhost';
