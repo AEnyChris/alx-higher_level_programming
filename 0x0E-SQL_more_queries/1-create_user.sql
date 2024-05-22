e7450ff84eff8802be07c7c59c51229ceb71a58b
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 00:54:26 +0100

    added task 1: create user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
--- creates user if it does not exist and grant all privileges
+-- creates user if it does not exist and grant all privileges to it
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
