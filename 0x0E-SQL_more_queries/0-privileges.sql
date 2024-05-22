cf368984c4323dba5a0c5244b39b9a109bf6f649
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 00:28:09 +0100

    first commit in this diretory

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,6 @@
+-- list the privileges given to specified users
+-- query to list privilege for user user_0d_1
+SHOW GRANT FOR user_0d_1@localhost;
+
+-- query to list privilege for user
+SHOW GRANT FOR user_0d_2@localhost;
