2671d1b8635653952e3d7de452d81e85c49fd9d6
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 23:58:04 +0100

    updated file

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- display average temperature from temperature table
 
-SELECT city, AVG(value) as avg_temp from temperature
+SELECT city, AVG(value) as avg_temp from temperatures
 GROUP BY city
 ORDER BY avg_temp DESC;
