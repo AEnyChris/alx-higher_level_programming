2f8f0d6e4d7e240bc0d34e98c93852d2d4be254b
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 23:55:00 +0100

    udated file

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,6 +4,8 @@ ALTER DATABASE hbtn_0c_0
 DEFAULT CHARACTER SET utf8mb4
 DEFAULT COLLATE = utf8mb4_unicode_ci;
 
+USE hbtn_0c_0;
+
 ALTER TABLE first_table
 DEFAULT CHARACTER SET utf8mb4 
 DEFAULT COLLATE utf8mb4_unicode_ci;
