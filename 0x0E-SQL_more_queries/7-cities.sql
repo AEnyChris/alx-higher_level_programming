26b8df31d40ab7a1769815b07d57a1cb1592a68a
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 02:22:57 +0100

    constraint added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,7 +4,7 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
-	name VARCHAR(256),
+	name VARCHAR(256) NOT NULL,
 	PRIMARY KEY (id),
 	FORIEGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
 );
