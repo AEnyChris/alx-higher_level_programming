4855084490c4af4d953c466873ee08e2ed7c0cf0
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 01:25:57 +0100

    syntax error correction

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- creates user and database if not exist and grant database privileges to user --
 
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
