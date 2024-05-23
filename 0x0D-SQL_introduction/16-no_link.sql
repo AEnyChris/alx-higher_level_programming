f731e93b142253dc5b6c766565b44bd41f46ffdc
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 23:52:15 +0100

    updated file

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,7 +1,7 @@
 -- lists all records of the table second_table of the database hbtn_0c_0
 -- rows without a name value should not be displayed
 
-SELECT * FROM second_table
+SELECT score, name FROM second_table
 WHERE name IS NOT NULL
 AND name != ''
 ORDER BY score DESC;
