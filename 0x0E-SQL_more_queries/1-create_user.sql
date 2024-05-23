48d443c650e4250146b7a2d7afc9a2063dfe3b08
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-23 01:09:43 +0100

    another update

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- creates user if it does not exist and grant all privileges to it
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
