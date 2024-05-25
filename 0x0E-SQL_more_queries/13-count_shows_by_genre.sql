9ec2abafea33bfdadeff82e99c57ffd23cc49d77
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-25 08:00:28 +0100

    met a requirement specification

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- list all genres and the number of tv shows linked to them
 
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
