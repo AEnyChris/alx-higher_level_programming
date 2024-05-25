f6e0c4e63f38132aa334b59c20c08f7a3f832d61
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-05-25 08:05:09 +0100

    correction

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,8 +1,13 @@
 -- list show title that are not Comedy in the database
 
-SELECT DISTINCT(tv_shows.title)
+
+SELECT tv_shows.title FROM tv_shows
+WHERE tv_shows.id NOT IN
+(SELECT tv_shows.id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name != 'Comedy' OR tv_genres.name IS NULL
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy")
 ORDER BY tv_shows.title;
