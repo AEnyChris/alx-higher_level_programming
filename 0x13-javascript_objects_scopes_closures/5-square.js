9357330f068ae7adde071687a35e8665c85ceab9
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-20 13:33:49 +0100

    added task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// Define a class Reactangle with fiels w and h
+// and a print, rotate and double method
+
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Sqaure extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+};
