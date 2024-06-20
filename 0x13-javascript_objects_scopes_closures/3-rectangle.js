9a2214c53a0949be703ccee7c006c5360e49348f
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-20 12:20:59 +0100

    style correction on task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -4,11 +4,12 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-  if (!(w <= 0 || w === undefined || h <= 0 || h === undefined)) {
-    this.width = w;
-    this.height = h;
+    if (!(w <= 0 || w === undefined || h <= 0 || h === undefined)) {
+      this.width = w;
+      this.height = h;
+    }
   }
- }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
