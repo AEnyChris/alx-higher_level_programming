cf44d072221706caff9f2205915e9141495df180
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-20 16:44:22 +0100

    added task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -4,7 +4,6 @@ exports.esrever = function (list) {
   const newArray = new Array(list.length);
   for (let i = 0; i < list.length; i++) {
     newArray[i] = list[list.length - (i + 1)];
-    console.log(i);
   }
   return newArray;
 };
