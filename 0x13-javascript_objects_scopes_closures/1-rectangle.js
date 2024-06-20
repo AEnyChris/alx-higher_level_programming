3aa3bc3002e1c0f63ebf024b5b8a4827834683ba
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-20 12:15:51 +0100

    style correction on task 1

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Define a class Reactangle with fiels w and h
 
-module.exports = class Rectangle{
+module.exports = class Rectangle {
   constructor (w, h) {
-  this.width = w;
-  this.height = h;
+    this.width = w;
+    this.height = h;
   }
 };
