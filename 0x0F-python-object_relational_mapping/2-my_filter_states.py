8c038214861c907ae19a7992af791c3fef90ddd4
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-22 04:19:27 +0100

    added task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""This module queries the states table
+and lists all its content according to condition
+"""
+
+import sys
+import MySQLdb
+
+
+def query_state(user, passwd, db, state_name):
+    """queries the state table to return all states"""
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306, user=user,
+            passwd=passwd,
+            db=db)
+
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states\
+        WHERE name LIKE BINARY '{}'\
+        ORDER BY id ASC".format(state_name))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    query_state(args[0], args[1], args[2], args[3])
