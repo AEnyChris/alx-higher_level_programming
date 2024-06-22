87c105cc2b4f7eb8bb672fad4af21f801615a532
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-22 11:46:08 +0100

    added task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,29 +10,27 @@ import re
 
 def query_state(user, passwd, db, state_name):
     """queries the state table to return all states"""
-    pattern = r'^[A-Z][a-z]+$'
-    if re.match(pattern, state_name):
-        db = MySQLdb.connect(
-                host="localhost",
-                port=3306, user=user,
-                passwd=passwd,
-                db=db)
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306, user=user,
+            passwd=passwd,
+            db=db)
 
-        cur = db.cursor()
+    cur = db.cursor()
 
-        cur.execute(
-            """
-            SELECT cities.name FROM cities\
-            JOIN states ON cities.state_id = states.id
-            WHERE states.name LIKE BINARY '{}'\
-            ORDER BY cities.id ASC""".format(state_name))
+    cur.execute(
+        """
+        SELECT cities.name FROM cities\
+        JOIN states ON cities.state_id = states.id
+        WHERE states.name=%s\
+        ORDER BY cities.id ASC""", {state_name})
 
-        rows = cur.fetchall()
-        city_list = [row[0] for row in rows if rows is not None]
-        print(", ".join(city_list))
+    rows = cur.fetchall()
+    city_list = [row[0] for row in rows if rows is not None]
+    print(", ".join(city_list))
 
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
 
 
 if __name__ == '__main__':
