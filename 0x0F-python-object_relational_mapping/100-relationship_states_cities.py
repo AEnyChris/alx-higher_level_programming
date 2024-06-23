398d932736d5df7f8cd1951dd1f76eba9b8d75bb
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-23 20:00:21 +0100

    added task 100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Adds a State and a City of the State
+to the database
+"""
+import sys
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy.orm.session import Session
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    conn_uri = f'mysql+mysqldb://{args[0]}:{args[1]}@localhost/{args[2]}'
+
+    engine = create_engine(conn_uri)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    new_state = State(name="California")
+    new_state.cities = [City(name="San Fransico")]
+    session.add(new_state)
+    session.commit()
+    session.close()
