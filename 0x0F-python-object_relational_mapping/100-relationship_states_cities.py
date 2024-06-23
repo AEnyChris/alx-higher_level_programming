bec90fb3d190a49b0dbc16f8a1fe8cce6cfbd74e
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-23 20:21:20 +0100

    added task 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     session = Session(engine)
     new_state = State(name="California")
-    new_city = City(name="San Fransico")
+    new_city = City(name="San Francisco")
     new_state.cities.append(new_city)
     session.add(new_state)
     session.commit()
