cb259f474fa4d60e71142aa55b56e278c793c5da
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-23 01:13:30 +0100

    added task 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm.session import Session
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    conn_uri = f'mysql+mysqldb://{args[0]}:{args[1]}@localhost/{args[2]}'
+
+    engine = create_engine(conn_uri)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    state = session.query(State).filter(State.id == 2).first()
+    state.name = "New Mexico"
+    session.commit()
+    session.close()
