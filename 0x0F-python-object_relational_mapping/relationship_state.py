de22490194ccd1ba117250df18b1a1d8ea6f14e4
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-06-23 20:01:36 +0100

    added supporting files for task 100

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""This module contains the class definition State
+and an instance of Base = declarative_base()
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+
+class State(Base):
+    """defines the class State inheriting from Base
+
+    Attributes:
+        __tablename__ (str): the name of table in database
+        id (int): unique, primary key, state id
+        name (str): name of states
+        cities (:obj:`City`): Cities of States
+
+    """
+    __tablename__ = 'states'
+    id = Column(
+            Integer,
+            primary_key=True,
+            nullable=False)
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', backref='state', cascade='all, delete-orphan')
