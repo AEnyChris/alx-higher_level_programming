29992471d92341459914888e5bd6ec43daa342fc
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-04 16:32:27 +0100

    added task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,5 +11,6 @@ def find_peak(list_of_integers):
             list_of_integers (list): a list or iterable
     """
     if list_of_integers:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
     return None
