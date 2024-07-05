547e2391001f4360d8d5d0e06e261172a6284479
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-05 11:12:11 +0100

    added task 5

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module makes a request to a given url
+using the requests module
+"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    resp = requests.get(sys.argv[1])
+
+    print(resp.headers["X-Request-Id"])
