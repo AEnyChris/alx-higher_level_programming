b8c000ed1a28ba45e7599dad49c079ea866f5835
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-31 00:09:45 +0100

    read a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,7 +5,7 @@ const filepath = process.argv[2];
 
 async function readFile (filepath) {
   try {
-    const data = await fs.readFile(filepath);
+    const data = await fs.readFile(filepath, 'utf8');
     console.log(data.toString());
   } catch (error) {
     console.error(error);
