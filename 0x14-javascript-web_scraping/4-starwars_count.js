a47c6122f18806bfaa3650fbc80d8e0e037ed412
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-31 03:47:33 +0100

    find Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,23 +3,27 @@
 const request = require('request');
 
 const mainUrl = process.argv[2];
-const searchUrl = 'https://swapi-api.alx-tools.com/api/people/?search=Wedge';
-let wedgeUrl;
+// const searchUrl = 'https://swapi-api.alx-tools.com/api/people/?search=Wedge';
+// let wedgeUrl;
 let count = 0;
 
-// Search for Wedge's url
+/* Search for Wedge's url
 request(searchUrl, function (error, response, body) {
   if (error) console.error(error);
   const data = JSON.parse(body);
   wedgeUrl = data.results[0].url;
 });
+*/
 
 // Count movies with Wedges's url
 request(mainUrl, function (error, response, body) {
   if (error) console.error(error);
   const data = JSON.parse(body);
   for (let i = 0; i < data.results.length; i++) {
-    if (data.results[i].characters.includes(wedgeUrl)) count++;
+    for (let j = 0; j < data.results[i].characters.length; j++) {
+      if (data.results[i].characters[j].includes('18')) count++;
+    }
+    // if (data.results[i].characters.includes(wedgeUrl)) count++;
   }
   console.log(count);
 });
