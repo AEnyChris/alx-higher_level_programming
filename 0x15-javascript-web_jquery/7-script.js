4dbd8664cb0bdddebfb6877ecb60b9c520187fed
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-08-08 02:22:59 +0100

    all scripts added at

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,3 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/people/2/?format=json', function (data) {
+  $('DIV#character').text(data.name);
+});
---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  for (const n in data.results) {
+    $('UL#list_movies').append(`<li>${data.results[n].title}</li>`);
+  }
+});
